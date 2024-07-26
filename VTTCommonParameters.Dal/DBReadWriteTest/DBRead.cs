1bc847ffececbaca0d8e7f80914c803917501ab0
Hayri Mert kurt <hayrimertkurt@outlook.com>
2024-07-26 15:16:58 +0300

    -Old implementation of ReadExcelData were removed. +ReadExcelData new implementation.

--- VTTCommonParameters.Dal/DBReadWriteTest/DBRead.cs
+++ VTTCommonParameters.Dal/DBReadWriteTest/DBRead.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MiniExcelLibs;
 using System.IO;
+using System.Data;
 
 namespace VTTCommonParameters.Dal.DBReadWriteTest
 {
@@ -12,31 +13,37 @@ namespace VTTCommonParameters.Dal.DBReadWriteTest
     {
         public void ReadExcelData(string filePath)
         {
-           // var rows= MiniExcel.Query(filePath).ToList().Skip(1);
-           // var headers= rows.Take(0).ToList();
-           //foreach (var header in headers)
-           // {
-           //     Console.WriteLine(header.A.ToString());
-           // }
-
-           // foreach (var row in rows)
-           // {
-
-           //     Console.WriteLine(row.B.ToString());
-                
-
-           // }
+            //_context.Projects.Add(new Project { Name = "Project1" });
+            //_context.Pages.Add(new Page { ProjectId=1(LINQ?)), Name = "Page1" }); iterate all pages first
+            
             var rows = MiniExcel.Query(filePath).ToList();
+            int rowId = 0;
             foreach (var row in rows)
             {
+                int paramId = 0;
+                int orderId = 0;
                 var rowDict = (IDictionary<string, object>)row;
                 foreach (var kvp in rowDict)
-                {
-                    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+                {   paramId++;
+                    orderId++;
+                    if (rowId==0)
+                    {
+
+
+                        Console.WriteLine($"OrderID:{orderId} ParamID:{paramId} Row:{rowId} Value:{kvp.Value} ");
+                        //_contetext.Parameters.Add(new Parameter {Id will be created automatically. I hope it creates starting with 1 and ++, ColumnName = kvp.Key, OrderId = orderId, DefaultValue = kvp.Value.ToString() });
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ParamID:{paramId} Row:{rowId} Value:{kvp.Value} ");
+                        //_context.ParameterValues.Add(new ParameterValue { ParameterId = paramId, RowId = rowId, Value = kvp.Value.ToString() });
+                    }
+                    
+
                 }
                 Console.WriteLine(); // For separating rows in output
+                rowId++;
             }
-
 
         }
 
